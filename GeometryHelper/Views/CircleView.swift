c552d9314c275b478baf7373785c8479242d3dd5
Hayden Chan <hchan@lcs.on.ca>
2023-11-17 11:52:42 -0500

    Made the slidebar and the Image in the design

--- GeometryHelper/Views/CircleView.swift
+++ GeometryHelper/Views/CircleView.swift
@@ -15,16 +15,31 @@ struct CircleView: View {
     // Mark: Computed properties
     var body: some View {
         VStack{
+            HStack {
+                Spacer()
+                Image("Image 1")
+                    .resizable()
+                    .scaledToFit()
+                .frame(width:250)
+            }
+            HStack{
+                Text("0")
+                Slider(value: $currentCircle.radius, in: 0...100)
+                Text("100")
+            }
             
-            Slider(value: $currentCircle.radius, in: 0...100)
             
-            List_item(title: "Radius", subtitle: "\(currentCircle.radius)")
+                List_item(title: "Radius", subtitle: "\(currentCircle.radius)")
+                .padding()
+                
+                List_item(title: "Diameter", subtitle: "\(currentCircle.diameter)")
+                .padding()
+                
+                List_item(title: "Perimeter", subtitle: "\(currentCircle.perimeter)")
+                .padding()
+                List_item(title: "Area", subtitle: "\(currentCircle.area)")
+                .padding()
             
-            List_item(title: "Diameter", subtitle: "\(currentCircle.diameter)")
-            
-            List_item(title: "Perimeter", subtitle: "\(currentCircle.perimeter)")
-            
-            List_item(title: "Area", subtitle: "\(currentCircle.area)")
            
            
             
